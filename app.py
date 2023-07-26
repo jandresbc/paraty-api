0cf5ac92c2eb266afca7e3b64138fe61e7fe0930
Julio Barrera <j.barreracarvajal@gmail.com>
2023-07-25 23:13:03 -0500

    nuevos datos

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from app.Resources.ReadDataResources import readData
 from app.Resources.ViewResources import viewData
 from flask_restful import Api
 
-app = Flask("paraty")
+app = Flask("app")
 app.config.from_object(Config)
 
 api = Api(app)
+db.init_app(app)
 migrate = Migrate(app,db)
 
 api.add_resource(readData,"/read")
@@ -17,5 +18,4 @@ api.add_resource(readData,"/read")
 api.add_resource(viewData,"/filter")
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(debug=Config.DEBUG)
